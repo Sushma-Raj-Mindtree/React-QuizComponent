dcb0d11722ce529b6f51efe8b5fe3d93716aff3e
Sushma Raj <M1033167@mindtree.com>
2020-01-03 14:58:01 +0530

    completed task

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
+  handleclick() {
+      this.props.clickHandler(this.props.button_text);
+  }
   render() {
     return (
       <li>
-        <button>{this.props.button_text}</button>
+        <button onClick={this.handleclick.bind(this)}>
+          {this.props.button_text}
+        </button>
       </li>
     );
   }
