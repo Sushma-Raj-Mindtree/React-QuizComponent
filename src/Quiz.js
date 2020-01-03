be5d7a79b4d48d44deb0d4d599c9b103d0e9a068
Sushma Raj <M1033167@mindtree.com>
2020-01-03 12:24:53 +0530

    completed task

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,8 +10,8 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
   render() {
-    const isQuizEnd = true;
-    //this.state.quiz_position - 1 === quizData.quiz_questions.length;
+    const isQuizEnd = //true;
+      this.state.quiz_position - 1 === quizData.quiz_questions.length;
     return (
       <div>
         {isQuizEnd ? (
